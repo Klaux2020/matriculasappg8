64e9b938bd27fb4dab0744e7157d542413714258
Klaux2020 <pervaldez.netscape.net>
2024-06-12 10:48:19 -0500

    stateles para devolver un widget

--- lib/pages/matricula_page.dart
+++ lib/pages/matricula_page.dart
@@ -11,33 +11,52 @@ class MatriculaPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Jhonny Gallegos"),
-            subtitle: Text("Tecsup"),
-            trailing: 
-              GestureDetector(
-              child: Icon(Icons.delete),
-              onTap: () {
-              print("Hola");
-            },
-            ),
-
-
-              // Otra Forma de Declarar el Boton(**)
-              // IconButton(
-              // icon: Icon(Icons.delete),
-              // onPressed: () {
-              //    print("Holas");
-              // },
-              // )
-            // Usar de Forma Tradicional (*)
-            // onTap: (){
-            //  print("Holas");
-            // },
-          )  
+          generateListTile("Jhony Gallegos", "TECSUP 01"),
+          GenerateListTile2(name: "Jhony", institution: "PUCP"),
         ],
         ),
     );
+  }
+}
+
+
+
+//Instanciando como Funcion (Impacta en el Proceso pues es muy pesada)
+Widget generateListTile(String name,String institution) {
+  return ListTile(
+            leading: Icon(Icons.person),
+            title: Text(name),
+            subtitle: Text(institution),
+            trailing:  IconButton(
+               icon: Icon(Icons.delete),
+               onPressed: () {
+                 print("Holas");
+               },
+               )
+            
+          ); 
+}
+
+//Instanciando como Clase (Impacta en el Proceso pues es muy Ligera)
+class GenerateListTile2 extends StatelessWidget {
+ String name="";
+ String institution="";
+
+  GenerateListTile2({
+    required this.name , 
+    required this.institution});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+            leading: Icon(Icons.person),
+            title: Text(name),
+            subtitle: Text(institution),
+            trailing:  IconButton(
+               icon: Icon(Icons.delete),
+               onPressed: () {
+                 print("Holas");
+               },
+               )
+          ); 
   }
 }
